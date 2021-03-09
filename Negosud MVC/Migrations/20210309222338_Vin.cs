c978497dedb4c6eeb5fb06bfff39096494a6850d
Caro <cfouquet.web@gmail.com>
2021-03-09 23:53:50 +0100

    display pictures in product list

---
+++ Negosud MVC/Migrations/20210309222338_Vin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Negosud_MVC.Migrations
+{
+    public partial class Vin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imgURL",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imgURL",
+                table: "Items");
+        }
+    }
+}
