5fdf6b60d905d7ca6c6cb55a4d5c70e5662abc02
Caro <cfouquet.web@gmail.com>
2021-03-10 15:22:41 +0100

    add view select list type and producer

--- Negosud MVC/Controllers/ItemsController.cs
+++ Negosud MVC/Controllers/ItemsController.cs
@@ -20,14 +20,22 @@ namespace Negosud_MVC.Controllers
         }
 
         // GET: Items
-        public async Task<IActionResult> Index(string searchString, string listYear)
+        public async Task<IActionResult> Index(string searchString, string listYear, string producer, string type)
         {
-            //search by type
+            //search by year
             IQueryable<string> yearQuery = from y in _context.Items
                                            orderby y.Year
                                            select y.Year;
+            //search by producer
+            IQueryable<string> producerQuery = from p in _context.Items
+                                           orderby p.Producer.Name
+                                               select p.Producer.Name;
 
-            //searche by string
+            IQueryable<string> typeQuery = from t in _context.Items
+                                           orderby t.Type.Name
+                                               select t.Type.Name;
+
+            //search by string
             var items = from m in _context.Items
                          select m;
 
@@ -36,20 +44,39 @@ namespace Negosud_MVC.Controllers
                 items = items.Where(s => s.Name.Contains(searchString));
             }
 
-            //search by type
+            
+            //search for select list in veiw
             if (!string.IsNullOrEmpty(listYear))
             {
                 items = items.Where(x => x.Year == listYear);
             }
 
+            if (!string.IsNullOrEmpty(producer))
+            {
+                items = items.Where(x => x.Producer.Name == producer);
+            }     
+            
+            if (!string.IsNullOrEmpty(type))
+            {
+                items = items.Where(x => x.Type.Name == type);
+            }
+
+            
             var productYearVM = new ProductYearViewModel
             {
+                Producers = new SelectList(await producerQuery.Distinct().ToListAsync()),
                 Years = new SelectList(await yearQuery.Distinct().ToListAsync()),
+                Types = new SelectList(await typeQuery.Distinct().ToListAsync()),
                 Items = await items.ToListAsync()
             };
 
-            /*var negosudContext = _context.Items.Include(i => i.Producer).Include(i => i.Supplier).Include(i => i.Type);*/
             return View(productYearVM);
+
+
+            //search by producer
+
+            /*var negosudContext = _context.Items.Include(i => i.Producer).Include(i => i.Supplier).Include(i => i.Type);*/
+
         }
 
         // GET: Items/Details/5
--- Negosud MVC/ViewModel/ProductYearViewModel.cs
+++ Negosud MVC/ViewModel/ProductYearViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Negosud_MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,13 @@ namespace Negosud_MVC.ViewModel
 {
     public class ProductYearViewModel
     {
-        public List<Models.Item> Items { get; set; }
+        public List<Item> Items { get; set; }
         public SelectList Years { get; set; }
+        public SelectList Types { get; set; }
+        public SelectList Producers { get; set; }
         public string ListYear { get; set; }
+        public string ListProducer { get; set; }
+        public string ListType { get; set; }
         public string SearchString { get; set; }
     }
 }
